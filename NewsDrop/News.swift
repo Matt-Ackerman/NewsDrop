d1ea546fcb2ee35fd7cde4446d03fdd132d48341
mattack <mattack@iastate.edu>
2018-04-08 12:09:59 -0500

    Initial commit. API call works and table creation works.

---
+++ NewsDrop/News.swift
@@ -0,0 +1,69 @@
+//
+//  News.swift
+//  TableViewTest
+//
+//  Created by Matt Ackerman on 4/8/18.
+//  Copyright © 2018 Matthew Ackerman. All rights reserved.
+//
+import Foundation
+
+struct News {
+    
+    let site:String
+    let title:String
+    static let url = "http://webhose.io/filterWebContent?token=42e28899-ec4a-4758-bf49-735c1eb6b793&format=json&ts=1523024626100&sort=crawled&q=site_type%3Anews%20performance_score%3A%3E9%20language%3Aenglish"
+    
+    // Initializes a News object using an array
+    init(json:[String:Any]) throws {
+        guard let site = json["site"] as? String else {throw SerializationError.missing("site is missing")}
+        guard let title = json["title"] as? String else {throw SerializationError.missing("title is missing")}
+        
+        self.site = site
+        self.title = title
+    }
+    
+    enum SerializationError:Error {
+        case missing(String)
+        case invalid(String, Any)
+    }
+    
+    // REST API call
+    static func getNews (completion: @escaping ([News]) -> ()) {
+        let request = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
+            
+            var newsArray:[News] = []
+            
+            // Getting json response and stepping down its heirarchy
+            if let data = data {
+                do {
+                    // top level
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                        // get all news postings in the API response
+                        if let posts = json["posts"] as? [[String:Any]] {
+                            
+                            // looping through all news postings
+                            for post in posts {
+                                if let thread = post["thread"] as? [String:Any] {
+                                    // creating a News object out of the "thread" object's JSON
+                                    if let newsObject = try? News(json: thread) {
+                                        newsArray.append(newsObject)
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+                // Return the array of News objects
+                completion(newsArray)
+            }
+        }
+        task.resume()
+    }
+    
+}
+
---
+++ NewsDrop/TableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  TableViewController.swift
+//  TableViewTest
+//
+//  Created by Matt Ackerman on 4/8/18.
+//  Copyright © 2018 Matthew Ackerman. All rights reserved.
+//
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    // articles gathered from api
+    var articles = [News]()
+    
+    // list of strings for the table with temp values until articles are gathered
+    var tableRows:[String] = ["", "", "", "", "", "", "", "", "", ""]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Asynchronously loading News articles
+        News.getNews { (results:[News]) in
+            for result in results {
+                print(result)
+                self.articles.append(result)
+            }
+            self.setText()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // sets our table rows to our articles
+    func setText() {
+        tableRows = [
+            articles[0].site + ": " +  articles[0].title,
+            articles[1].site + ": " +  articles[1].title,
+            articles[2].site + ": " +  articles[2].title,
+            articles[3].site + ": " +  articles[3].title,
+            articles[4].site + ": " +  articles[4].title,
+            articles[5].site + ": " +  articles[5].title,
+            articles[6].site + ": " +  articles[6].title,
+            articles[7].site + ": " +  articles[7].title,
+            articles[8].site + ": " +  articles[8].title,
+            articles[9].site + ": " +  articles[9].title
+        ]
+        reloadTableWithNews()
+    }
+
+    // return how many rows we want in our table
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableRows.count
+    }
+
+    // set cells to our tableRows
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = String(tableRows[indexPath.row])
+        return cell
+    }
+    
+    // reloads table once we have the news
+    @objc func reloadTableWithNews() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+}
